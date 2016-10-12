f349d39540500ecb9fc40c046916105b583fcc32
rthor <rthor@LAPTOP-343NT6OO>
2016-10-12 16:10:36 -0700

    followed lesson to conclusion.

---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,8 @@
+﻿namespace Texter.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "AC55a42b8c4fd54b53488c1b1f7a2f00f9";
+        public static string AuthToken = "3f58b4431a4a20d89d49c31832bdac52";
+    }
+}
---
+++ src/Texter/Models/TexterContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Texter.Models
+{
+    public class TexterContext : DbContext
+    {
+        public virtual DbSet<Message> Messages
+        {
+            get; set;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Texter;integrated security=True");
+        }
+    }
+}
